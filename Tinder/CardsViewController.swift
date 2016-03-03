aff0d89d9400fff713a4a4d6ab26c0a7a5ae9e67
Lee Edwards <lee.edwards@teespring.com>
2016-03-02 19:16:15 -0800

    Milestone 1

---
+++ Tinder/CardsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Lee Edwards on 3/2/16.
+//  Copyright © 2016 Lee Edwards. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
