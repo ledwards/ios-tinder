0f28eef450ca71cb76fe023cdb53232a2116eed6
Lee Edwards <lee.edwards@teespring.com>
2016-03-02 19:27:52 -0800

    Milestone 2

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class CardsViewController: UIViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    
+    @IBAction func onPan(sender: AnyObject) {
+        let translation = sender.translationInView(view)
+        
+        if sender.state == .Began {
+            originalProfileCenter = profileImageView.center
+        } else if sender.state == .Changed {
+            profileImageView.center = CGPoint(x: originalProfileCenter!.x + translation.x, y: originalProfileCenter!.y)
+        } else if sender.state == .Ended {
+            
+        }
+        
+    }
+    
+    var originalProfileCenter: CGPoint?
 
     override func viewDidLoad() {
         super.viewDidLoad()
